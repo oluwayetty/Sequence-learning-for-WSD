4dc494738af749246fc106f27a6d097a65f11f44
Oluwayetty <oluwayetti@gmail.com>
2019-08-26 12:01:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# NLP Homework 3
+
+## Submission skeleton
+```
+- code
+  |__ predict.py # This file contains all the functions you have to implement.
+- resources # mappings are in here. you should place any additional resource (e.g. trained models) in here
+  |__ babelnet2lexnames.tsv  # bnids to lexnames
+  |__ babelnet2wndomains.tsv # bnids to WordNet domains labels
+  |__ babelnet2wordnet       # bnids to WordNet offsets
+- README.md # this file
+- Homework_3_nlp.pdf # the slides presenting this homework
+- report.pdf	# your report
+```
+
+## Instructions
+Place all your code in the `code` folder. You can create other files.
+Place any additional resources needed for the functions in `predict.py` (such as the weights of your trained models) in the `resources` folder.
+Place your report as `report.pdf` in the root folder.
+Follow the slides for any additional information.
---
+++ code/predict.py
@@ -0,0 +1,60 @@
+
+
+def predict_babelnet(input_path : str, output_path : str, resources_path : str) -> None:
+    """
+    DO NOT MODIFY THE SIGNATURE!
+    This is the skeleton of the prediction function.
+    The predict function will build your model, load the weights from the checkpoint and write a new file (output_path)
+    with your predictions in the "<id> <BABELSynset>" format (e.g. "d000.s000.t000 bn:01234567n").
+    
+    The resources folder should contain everything you need to make the predictions. It is the "resources" folder in your submission.
+    
+    N.B. DO NOT HARD CODE PATHS IN HERE. Use resource_path instead, otherwise we will not be able to run the code.
+    If you don't know what HARD CODING means see: https://en.wikipedia.org/wiki/Hard_coding
+
+    :param input_path: the path of the input file to predict in the same format as Raganato's framework (XML files you downloaded).
+    :param output_path: the path of the output file (where you save your predictions)
+    :param resources_path: the path of the resources folder containing your model and stuff you might need.
+    :return: None
+    """
+    pass
+
+
+def predict_wordnet_domains(input_path : str, output_path : str, resources_path : str) -> None:
+    """
+    DO NOT MODIFY THE SIGNATURE!
+    This is the skeleton of the prediction function.
+    The predict function will build your model, load the weights from the checkpoint and write a new file (output_path)
+    with your predictions in the "<id> <wordnetDomain>" format (e.g. "d000.s000.t000 sport").
+
+    The resources folder should contain everything you need to make the predictions. It is the "resources" folder in your submission.
+
+    N.B. DO NOT HARD CODE PATHS IN HERE. Use resource_path instead, otherwise we will not be able to run the code.
+    If you don't know what HARD CODING means see: https://en.wikipedia.org/wiki/Hard_coding
+
+    :param input_path: the path of the input file to predict in the same format as Raganato's framework (XML files you downloaded).
+    :param output_path: the path of the output file (where you save your predictions)
+    :param resources_path: the path of the resources folder containing your model and stuff you might need.
+    :return: None
+    """
+    pass
+
+
+def predict_lexicographer(input_path : str, output_path : str, resources_path : str) -> None:
+    """
+    DO NOT MODIFY THE SIGNATURE!
+    This is the skeleton of the prediction function.
+    The predict function will build your model, load the weights from the checkpoint and write a new file (output_path)
+    with your predictions in the "<id> <lexicographerId>" format (e.g. "d000.s000.t000 noun.animal").
+
+    The resources folder should contain everything you need to make the predictions. It is the "resources" folder in your submission.
+
+    N.B. DO NOT HARD CODE PATHS IN HERE. Use resource_path instead, otherwise we will not be able to run the code.
+    If you don't know what HARD CODING means see: https://en.wikipedia.org/wiki/Hard_coding
+
+    :param input_path: the path of the input file to predict in the same format as Raganato's framework (XML files you downloaded).
+    :param output_path: the path of the output file (where you save your predictions)
+    :param resources_path: the path of the resources folder containing your model and stuff you might need.
+    :return: None
+    """
+    pass
